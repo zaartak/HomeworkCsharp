abb92ad73c7d4923ef3dec28e28ac0a73dc080f2
Zaartak <Zaartak1994@gmail.com>
2023-06-19 22:20:20 +0300

    Cubes solved

---
+++ Task_new_2/cubes_numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число, таблицу кубов которого вы хотите вывести в консоль: ");
+int number = int.Parse(Console.ReadLine()!);
+
+for( int i = 1; i <= number; i++)
+{
+    int coub = Convert.ToInt32(Math.Pow(i, 3));
+    Console.WriteLine(coub);
+}
