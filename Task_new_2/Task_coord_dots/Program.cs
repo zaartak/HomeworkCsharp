da591ea5a8fbb59165a51d3bf4eda72fd5b346a6
Zaartak <Zaartak1994@gmail.com>
2023-06-19 21:51:00 +0300

    solved coord_dots

---
+++ Task_new_2/Task_coord_dots/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите координату x точки A: ");
+int ax = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату y точки A: ");
+int ay = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату z точки A: ");
+int az = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату x точки B: ");
+int bx = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату y точки B: ");
+int by = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите координату z точки B: ");
+int bz = Convert.ToInt32(Console.ReadLine()!);
+
+double AB = Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2) + Math.Pow((bz - az), 2));
+
+Console.WriteLine($"Растояние между AB = {AB:f2}");
