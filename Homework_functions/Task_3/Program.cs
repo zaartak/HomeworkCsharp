c1dedfba713dfa493c3710d620ac113bd2a9eab3
Zaartak <Zaartak1994@gmail.com>
2023-06-22 22:02:32 +0300

    solve task 3

---
+++ Homework_functions/Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран. (можно указать любой промежуток)
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] GetArray(int size)
+{
+    int [] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0,1000);
+    }
+    return result;
+}
+
+int[] array = GetArray(5);
+Console.WriteLine($"[{String.Join(",",array)}]");
